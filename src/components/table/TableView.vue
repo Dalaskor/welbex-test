da5a87374d0c49b35d26627219efb3617dfd9e3b
Dalaskor <dalas.prog@gmail.com>
2022-09-28 12:44:07 +0400

    Add filters

--- src/components/table/TableView.vue
+++ src/components/table/TableView.vue
@@ -1,5 +1,17 @@
 <template lang="pug">
 .table-view
+    .filters
+        select(v-model="propertySelect").filters__input.filters__input_property
+            option(value="date") Дата
+            option(value="name") Название
+            option(value="count") Количество
+            option(value="distance") Расстояние
+        select(v-model="signSelect").filters__input.filters__input_sign
+            option(value="equal") =
+            option(value="contain") ~
+            option(value="more") &gt;
+            option(value="less") &lt;
+        input(type="text", v-model="conditionInput").filters__input.filters__input_condition
     .table
         .table__header
             .table__cell.table__cell_header
@@ -66,22 +78,88 @@ export default {
             isSortByCountDown: false,
             isSortByDistance: false,
             isSortByDistanceDown: false,
+            propertySelect: '',
+            signSelect: '',
+            conditionInput: '',
         };
     },
     computed: {
         ...mapGetters([
             'POSTS',
         ]),
+        filteredPosts() {
+            if (this.propertySelect === "" || this.signSelect === "" || this.conditionInput === "") {
+                return this.POSTS;
+            } else {
+                let condition = this.conditionInput;
+                switch(this.propertySelect) {
+                case 'date':
+                    if (this.signSelect === "equal") {
+                        return this.POSTS.filter(elem => elem.date.toLowerCase() === String(condition).toLowerCase());
+                    }
+                    if (this.signSelect === "more") {
+                        return this.POSTS.filter(elem => elem.date.toLowerCase() > String(condition).toLowerCase());
+                    }
+                    if (this.signSelect === "less") {
+                        return this.POSTS.filter(elem => elem.date.toLowerCase() < String(condition).toLowerCase());
+                    }
+                    if (this.signSelect === "contain") {
+                        return this.POSTS.filter(elem => elem.date.includes(String(condition)));
+                    }
+                case 'name':
+                    if (this.signSelect === "equal") {
+                        return this.POSTS.filter(elem => elem.name.toLowerCase() === String(condition).toLowerCase());
+                    }
+                    if (this.signSelect === "more") {
+                        return this.POSTS.filter(elem => elem.name.toLowerCase() > String(condition).toLowerCase());
+                    }
+                    if (this.signSelect === "less") {
+                        return this.POSTS.filter(elem => elem.name.toLowerCase() < String(condition).toLowerCase());
+                    }
+                    if (this.signSelect === "contain") {
+                        return this.POSTS.filter(elem => elem.name.includes(String(condition)));
+                    }
+                case 'count':
+                    if (this.signSelect === "equal") {
+                        return this.POSTS.filter(elem => elem.count === Number(condition));
+                    }
+                    if (this.signSelect === "more") {
+                        return this.POSTS.filter(elem => elem.count > Number(condition));
+                    }
+                    if (this.signSelect === "less") {
+                        return this.POSTS.filter(elem => elem.count < Number(condition));
+                    }
+                    if (this.signSelect === "contain") {
+                        return this.POSTS.filter(elem => String(elem.count).includes(String(condition)));
+                    }
+                case 'distance':
+                    if (this.signSelect === "equal") {
+                        return this.POSTS.filter(elem => elem.distance === Number(condition));
+                    }
+                    if (this.signSelect === "more") {
+                        return this.POSTS.filter(elem => elem.distance > Number(condition));
+                    }
+                    if (this.signSelect === "less") {
+                        return this.POSTS.filter(elem => elem.distance < Number(condition));
+                    }
+                    if (this.signSelect === "contain") {
+                        return this.POSTS.filter(elem => String(elem.distance).includes(String(condition)));
+                    }
+            };
+            }
+        },
         pages() {
             const COUNT = 10;
 
-            return Math.ceil(this.POSTS.length / COUNT);
+            // return Math.ceil(this.POSTS.length / COUNT);
+            return Math.ceil(this.filteredPosts.length / COUNT);
         },
         paginatedPosts() {
             const from = (this.pageNumber - 1) * this.postsPerPage;
             const to = from + this.postsPerPage;
 
-            return this.POSTS.slice(from, to);
+            // return this.POSTS.slice(from, to);
+            return this.filteredPosts.slice(from, to);
         },
     },
     methods: {
@@ -91,6 +169,7 @@ export default {
         pageClick(page) {
             this.pageNumber = page;
         },
+
         clearSort() {
             this.isSortByName = false;
             this.isSortByNameDown = false;
@@ -146,6 +225,39 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.filters {
+    max-width: 76.8rem;
+    margin: 0 auto;
+    margin-top: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+
+    &__input {
+        width: 12rem;
+        border: solid 0.1rem #999999;
+        background-color: #444444;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 1.2rem;
+        line-height: 1.6rem;
+        color: #FFFFFF;
+        padding-left: 1rem;
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        border-radius: 0.5rem;
+
+        &_property {}
+
+        &_sign {
+            width: 5rem;
+        }
+
+        &_condition {}
+    }
+}
 .table {
     max-width: 76.8rem;
     border: 0.3rem solid #999999;
@@ -153,7 +265,7 @@ export default {
     overflow: hidden;
     width: 100%;
     margin: 0 auto;
-    margin-top: 3rem;
+    margin-top: 1.5rem;
     background-color: #444444;
 
     &__header {
